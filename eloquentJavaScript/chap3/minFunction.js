1f8face21a4f0a393406d8bd5fe2619d47bd8ca6
Steve Roma <sjroma@hotmail.com>
2017-05-21 22:54:52 -0400

    chap3: added comments to minFunction.js

--- eloquentJavaScript/chap3/minFunction.js
+++ eloquentJavaScript/chap3/minFunction.js
@@ -1,3 +1,6 @@
+// Math.min is a function that returns the smallest argument.
+// Write a function min that takes two arguments and returns the minimum.
+
 function min(x1, x2) {
   if (x1 < x2)
     return x1;
