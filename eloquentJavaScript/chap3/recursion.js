6af5f819116062e2809d3afc1ec742a5a34ed3a3
Steve Roma <sjroma@hotmail.com>
2017-05-22 00:38:02 -0400

    prework: chap3, recursion initial check in

---
+++ eloquentJavaScript/chap3/recursion.js
@@ -0,0 +1,17 @@
+// Modulo (%) can be used to test if a number is even or odd by using % 2
+// to check whether it's divisible by two.
+// Another way is;
+// Zero is even, One is odd, for any other number N, its evenness is the
+// same as N - 2.
+// Define a recursive function isEven corresponding to this description.
+// The function should accept a number parameter and return a Boolean.
+
+function isEven(N) {
+  if (N == 0)
+    return true;
+  else if (N == 1)
+    return false;
+  else isEven(N - 2);
+}
+
+console.log(isEven(1));
