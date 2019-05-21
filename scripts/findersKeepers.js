55bf4536a3d61d36881b9b4be79b7033436d2217
Steve Roma <sjroma@hotmail.com>
2019-05-21 18:37:57 -0400

    comment all console.log outputs except one

--- scripts/findersKeepers.js
+++ scripts/findersKeepers.js
@@ -13,6 +13,6 @@ function findElement(arr, func) {
   }
 }
 
-console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
-console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })); //should return 8.
-console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })); //should return undefined.
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0)); //should return 2
+//console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })); //should return 8.
+//console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })); //should return undefined.
--- scripts/titleCaseSentence.js
+++ scripts/titleCaseSentence.js
@@ -13,6 +13,6 @@ function titleCase(str) {
     .join(' ');
  }
 
-console.log(titleCase("I'm a little tea pot")); //should return I'm A Little Tea Pot
+//console.log(titleCase("I'm a little tea pot")); //should return I'm A Little Tea Pot
 console.log(titleCase("sHoRt AnD sToUt")); //should return Short And Stout
-console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")); //should return Here Is My Handle Here Is My Spout
+//console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")); //should return Here Is My Handle Here Is My Spout
